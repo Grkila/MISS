f0037eba8e68aa7477642b5a3ed219f1506304fa
Grkila <64324148+Grkila@users.noreply.github.com>
2022-10-22 20:10:55 +0200

    Delete Zadaci za vezbu directory

--- Zadaci za vezbu/1. zadatak.jl
+++
@@ -1,4 +0,0 @@
-#Za proizvoljnu kvadratnu matricu A, izdvojiti sve parne kolone.
-function zad1(A)
-    return kolona=A[:,2:2:end]
-end
--- Zadaci za vezbu/2. zadatak.jl
+++
@@ -1,5 +0,0 @@
-#Za proizvoljnu kvadratnu matricu A, izdvojiti sve elemente koji su deljivi sa 9.
-function zad2(A)
-    deljivi= A[A.%9 .== 0 .&& A .!=0] #iz matrice A izdvoj svaku kojoj je ostatak deljenja sa 9 nula i koja nije jednaka nuli(izbacujemo same nule)
-    return deljivi
-end
